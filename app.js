397d5455395a615c77d760200235bc4ebb0b4ed2
miyemicodes <lizzymiyemi@gmail.com>
2023-11-26 19:48:16 -0800

    commit

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function toggleAccordion(){
   if (itemToggle == "false") {
     this.setAttribute("aria-expanded", "true");
   }
+
    item.addEventListener("click", toggleAccordion);
 }
-*/
 
 const accBtn = document.querySelectorAll(".tick button");
 const panel = document.querySelectorAll(".acc-drop-panel");
@@ -28,3 +28,53 @@ for (let i = 0; i < accBtn.length; i++) {
   });
   
 }
+*/
+function app() {
+ 
+  const plansBtn = document.querySelector(".plan-close-icon");
+  const plansSection = document.querySelector(".shopify-plans");
+  const profileMenuBtn = document.querySelector("#profile-drop");
+  const profileMenuContent = document.querySelector("#profile-dropdown");
+  const profileMenuItems = document.querySelectorAll(".p-menu");
+  
+
+  plansBtn.addEventListener("click",e =>{
+
+    plansSection.style.display = "none";
+
+  });
+  console.log(plansSection);
+
+  function closeProfileMenu (){
+    profileMenuBtn.ariaExpanded = "false";
+    profileMenuBtn.focus();
+  }
+
+
+  function openProfileMenu(){
+    profileMenuBtn.ariaExpanded = "true";
+    profileMenuItems.item(0).focus();
+  }
+
+  function toggleProfileMenu() {
+
+    const profileExpanded =
+    profileMenuBtn.attributes["aria-expanded"].value ===
+      "true";
+    profileMenuContent.classList.toggle("profile-active");
+
+    if (profileExpanded) {
+      closeProfileMenu();
+    } else {
+      openProfileMenu();
+    }
+  }
+
+  profileMenuBtn.addEventListener("click", toggleProfileMenu);
+
+
+}
+
+app();
+
+
--- index.js
+++
@@ -1,7 +0,0 @@
-function(){
- const menuTrigger = document.querySelector("#profile-menu");
-console.log(menuTrigger);
-
-}
-
-app();
